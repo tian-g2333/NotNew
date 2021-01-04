3962bb37310ee54fabe12473dec94de3e2be3e0d
tian-g2333 <1129327898@qq.com>
2021-01-04 21:43:00 +0800

    登录注册界面

--- app/src/main/java/com/android/hys/notnew/MainActivity.java
+++ app/src/main/java/com/android/hys/notnew/MainActivity.java
@@ -2,13 +2,105 @@ package com.android.hys.notnew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.hys.notnew.service.UserService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Boolean bPwdSwitch = false;
+    private EditText etPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //登录
+        Button signin = (Button) findViewById(R.id.signin);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String name = ((EditText) findViewById(R.id.et_user)).getText().toString();
+                String password = ((EditText) findViewById(R.id.et_pwd)).getText().toString();
+                if (UserService.signIn(name, password,"0000"))
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MainActivity.this, Activity_home.class);
+                            intent.putExtra("extra_name",name);
+                            startActivity(intent);
+                        }
+                    });
+                else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "登录失败", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+
+
+        //注册
+        Button signup = (Button) findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, Activity_LoginUp.class);
+                startActivity(intent);
+
+                /*String name = ((EditText) findViewById(R.id.et_user)).getText().toString();
+                String password = ((EditText) findViewById(R.id.et_pwd)).getText().toString();
+                if (UserService.signUp(name, password))
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "注册失败", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }*/
+            }
+        });
+
+        //切换密码明文显示
+        final ImageView ivPwdSwitch = findViewById(R.id.iv_pwd_switch);
+        etPwd = findViewById(R.id.et_pwd);
+        ivPwdSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bPwdSwitch = !bPwdSwitch;
+                if (bPwdSwitch) {
+                    ivPwdSwitch.setImageResource(R.drawable.ic_baseline_visibility_24);
+                    etPwd.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                } else {
+                    ivPwdSwitch.setImageResource(R.drawable.ic_baseline_visibility_off_24);
+                    etPwd.setInputType(
+                            InputType.TYPE_TEXT_VARIATION_PASSWORD |
+                                    InputType.TYPE_CLASS_TEXT);
+                    etPwd.setTypeface(Typeface.DEFAULT);
+                }
+            }
+        });
     }
 }
